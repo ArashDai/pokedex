a65e9d6bc0d2b2480ff9ec9af67ee47eced6a52f
Arash <arashdai@gmail.com>
2016-12-23 21:34:28 -0800

    test

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ var About = React.createClass({
       
       <div className='container-fluid Card'>
 
-        <h2 className='text-center'> About </h2>
+        <h2 className='text-center'>About</h2>
 
         <p className='text-center col-xs-12 col-sm-6 col-sm-offset-3'>
           This application is built with React.js using the v2 pokeAPI
